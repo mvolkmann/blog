3bd42a6974d6c618f8c95837fe89b6c66bf243fc
mvolkmann <mvolkmann@users.noreply.github.com>
2020-05-03 12:58:52 +0000

    deploy: af892900599c8b8913fc976e0eac2af83d628c80

--- js/topics.js
+++ js/topics.js
@@ -1,12 +1,4 @@
-// Only register the service worker when not on localhost.
-if (location.hostname !== 'localhost' && 'serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/blog/service-worker.js');
-  });
-}
-
-let nav;
-let navLeftWhenClosed;
+/* global navLeftWhenClosed: false */
 
 /**
  * This is used as the click handler for topic links.
@@ -46,27 +38,7 @@ function iframeLoaded(iframe) {
 
 // eslint-disable-next-line no-unused-vars
 function toggleHamburgerMenu() {
+  const nav = document.querySelector('nav');
   const isOpen = nav.style.left === '0px';
   nav.style.left = isOpen ? navLeftWhenClosed : 0;
 }
-
-window.onload = () => {
-  // If the URL contains a hash, use it to
-  // restore the proper page in the iframe.
-  const {hash} = location;
-  if (hash) {
-    const iframe = document.querySelector('iframe');
-    iframe.src = hash.substring(1); // removes leading #
-  }
-
-  nav = document.querySelector('nav');
-  if (nav) {
-    navLeftWhenClosed = '-' + nav.getBoundingClientRect().width + 'px';
-    nav.style.left = navLeftWhenClosed; // to initially hide it
-  }
-
-  const mainLinks = Array.from(document.querySelectorAll('.main-links > a'));
-  for (const a of mainLinks) {
-    if (a.href === location.href) a.classList.add('selected');
-  }
-};
