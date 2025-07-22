7449361e72a7c33b310159f136ef9c7f86a51838
mvolkmann <mvolkmann@users.noreply.github.com>
2025-07-22 16:02:33 +0000

    deploy: c897be3be517eb5c454ba74a8a73681ee18e2286

---
+++ js/number-slider.js
@@ -0,0 +1,38 @@
+import Wrec, {css, html} from './wrec.js';
+
+class NumberSlider extends Wrec {
+  static properties = {
+    label: {type: String},
+    labelWidth: {type: String},
+    max: {type: Number, value: 100},
+    min: {type: Number, value: 0},
+    value: {type: Number}
+  };
+
+  static css = css`
+    :host {
+      display: flex;
+      align-items: center;
+      gap: 0.5rem;
+    }
+
+    input[type='number'] {
+      width: 6rem;
+    }
+
+    label {
+      --label-width: this.labelWidth;
+      font-weight: bold;
+      text-align: right;
+      width: var(--label-width);
+    }
+  `;
+
+  static html = html`
+    <label>this.label</label>
+    <input type="range" min="this.min" max="this.max" value="this.value" />
+    <span>this.value</span>
+  `;
+}
+
+NumberSlider.register();
