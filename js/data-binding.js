d75f6526ba8e5a7effee23250c3ba7e29f2785a4
mvolkmann <mvolkmann@users.noreply.github.com>
2025-07-22 19:36:10 +0000

    deploy: 56f4e0b5ba2a9bc59045db5853691ba268f21359

---
+++ js/data-binding.js
@@ -0,0 +1,68 @@
+import Wrec, {css, html} from './wrec.js';
+
+const capitalize = str =>
+  str ? str.charAt(0).toUpperCase() + str.slice(1) : str;
+
+class DataBinding extends Wrec {
+  static properties = {
+    color: {type: String},
+    colors: {type: String, required: true},
+    labels: {
+      type: String,
+      computed: 'this.getLabels()',
+      uses: 'colors'
+    },
+    size: {type: Number, value: 18}
+  };
+
+  static css = css`
+    :host {
+      display: flex;
+      flex-direction: column;
+      gap: 0.5rem;
+      font-family: sans-serif;
+    }
+    p {
+      --color: this.color;
+      --size: this.size;
+      color: var(--color);
+      font-size: calc(var(--size) * 1px);
+      margin: 6px 0;
+    }
+  `;
+
+  static html = html`
+    <div>
+      <label>Color Options (comma-separated):</label>
+      <input value="this.colors" />
+    </div>
+    <radio-group
+      name="color1"
+      labels="this.labels"
+      value="this.color"
+      values="this.colors"
+    ></radio-group>
+    <select-list
+      name="color2"
+      labels="this.labels"
+      value="this.color"
+      values="this.colors"
+    ></select-list>
+    <number-slider
+      label="Size"
+      max="48"
+      min="12"
+      value="this.size"
+    ></number-slider>
+    <p>You selected the color <span id="selected-color">this.color</span>.</p>
+  `;
+
+  getLabels() {
+    return this.colors
+      .split(',')
+      .map(color => capitalize(color))
+      .join(',');
+  }
+}
+
+DataBinding.register();
---
+++ js/radio-group.js
@@ -0,0 +1,86 @@
+import Wrec, {css, html} from './wrec.js';
+
+class RadioGroup extends Wrec {
+  static formAssociated = true;
+
+  static properties = {
+    default: {type: String},
+    labels: {type: String},
+    name: {type: String, required: true},
+    values: {type: String, required: true},
+    value: {type: String}
+  };
+
+  static css = css`
+    :host > div {
+      display: flex;
+      gap: 0.5rem;
+
+      > div {
+        display: flex;
+        align-items: center;
+      }
+    }
+  `;
+
+  static html = html`
+    <div>
+      this.values.split(",").map((value, index) => this.makeRadio(value, index,
+      this.labels )).join("")
+    </div>
+  `;
+
+  connectedCallback() {
+    super.connectedCallback();
+    if (!this.default) this.default = this.values.split(',')[0];
+    if (!this.value) this.value = this.default;
+    this.#fixValue();
+  }
+
+  attributeChangedCallback(attrName, oldValue, newValue) {
+    super.attributeChangedCallback(attrName, oldValue, newValue);
+    if (attrName === 'value') {
+      // Update the checked state of the radio buttons.
+      const inputs = this.shadowRoot.querySelectorAll('input');
+      for (const input of inputs) {
+        input.checked = input.value === newValue;
+      }
+    } else if (attrName === 'values') {
+      this.#fixValue();
+    }
+  }
+
+  // This handles the case when the specified value
+  // is not in the list of values.
+  #fixValue() {
+    requestAnimationFrame(() => {
+      const values = this.values.split(',');
+      if (!values.includes(this.value)) this.value = values[0];
+    });
+  }
+
+  handleChange(event) {
+    this.value = event.target.value;
+  }
+
+  makeRadio(value, index) {
+    let label = this.labels.split(',')[index];
+    if (!label) return '';
+    value = value.trim();
+    return html`
+      <div>
+        <input
+          type="radio"
+          id="${value}"
+          name="${this.name}"
+          onchange="handleChange"
+          value="${value}"
+          ${value === this.value ? 'checked' : ''}
+        />
+        <label for="${value}">${label}</label>
+      </div>
+    `;
+  }
+}
+
+RadioGroup.register();
