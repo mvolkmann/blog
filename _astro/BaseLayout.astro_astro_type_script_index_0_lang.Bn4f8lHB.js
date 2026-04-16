19a0c61a9640e9e3f9286f78d994e80e82643232
mvolkmann <mvolkmann@users.noreply.github.com>
2026-04-16 02:33:44 +0000

    deploy: 1fbafbd655eb1f01a2162297d3c6586171ab7084

--- _astro/BaseLayout.astro_astro_type_script_index_0_lang.Bn4f8lHB.js
+++
@@ -1,1 +0,0 @@
-import"./TopicLayout.astro_astro_type_script_index_0_lang.1kNvo-QB.js";
--- js/topics.js
+++ js/topics.js
@@ -1,63 +1,24 @@
-/* global navLeftWhenClosed: false */
+/* global toggleHamburgerMenu: false */
 
 /**
- * This is used as the click handler for topic links.
+ * This handles clicks on topic navigation links.
  */
 // eslint-disable-next-line no-unused-vars
-function handleLinkClick(link, url) {
-  // If the URL starts with "http", open it in a new window.
-  if (url.startsWith('http')) {
-    window.open(url);
-    return;
-  }
-
-  // Only toggle the "expanded" class for non-leaf links.
-  // Links for leaf nodes do not have a next sibling.
-  if (link.nextElementSibling) {
+function handleLinkClick(event, link, hasChildren) {
+  if (hasChildren) {
+    event.preventDefault();
     link.classList.toggle('expanded');
-  } else {
-    // Only change the main content area if a leaf link was clicked.
-    const activeLink = document.querySelector('.active');
-
-    // If the clicked link is not already the active one ...
-    if (link !== activeLink) {
-      // If there's a previous active link, style it as no longer active.
-      if (activeLink) activeLink.classList.remove('active');
-
-      // Style the clicked link as active.
-      link.classList.add('active');
-
-      const iframe = document.querySelector('iframe');
-      iframe.style.opacity = 0;
-      setTimeout(() => (iframe.src = url), 300); // matches transition-duration in assets/styles.css
-
-      // Put new page URL in URL hash so it can be bookmarked.
-      const {origin} = location;
-      location.hash = url.startsWith(origin)
-        ? url.substring(origin.length)
-        : url;
-    }
-
-    // Only toggle hamburger menu if a leaf link was clicked.
-    toggleHamburgerMenu();
+    return false;
   }
+
+  toggleHamburgerMenu();
+  return true;
 }
 
-// eslint-disable-next-line no-unused-vars
-function iframeLoaded(iframe) {
-  iframe.style.opacity = 1;
-}
-
-// This is called from topics-layout.njk.
+/**
+ * This prints the current page.
+ */
 // eslint-disable-next-line no-unused-vars
 function print() {
-  document.getElementById('topic-content').contentWindow.print();
-}
-
-// eslint-disable-next-line no-unused-vars
-function toggleHamburgerMenu() {
-  const nav = document.querySelector('nav');
-  const {left} = nav.style;
-  const isOpen = !left || left === '0px';
-  nav.style.left = isOpen ? navLeftWhenClosed : 0;
+  window.print();
 }
